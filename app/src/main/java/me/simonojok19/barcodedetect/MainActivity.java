95bdf83c5ce53012131842fc18033b5b147345b8
simonojok19 <simonojok19@gmail.com>
2020-04-13 11:30:02 +0300

    done

--- app/src/main/java/me/simonojok19/barcodedetect/MainActivity.java
+++ app/src/main/java/me/simonojok19/barcodedetect/MainActivity.java
@@ -2,7 +2,18 @@ package me.simonojok19.barcodedetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView imageView = findViewById(R.id.imageView);
+                TextView textView = findViewById(R.id.textContent);
+                Bitmap bitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.puppy
+                );
+                imageView.setImageBitmap(bitmap);
+                BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+
+                if (!detector.isOperational()) {
+                    textView.setText("Could not set up the detector");
+                }
+
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<Barcode> barCodes = detector.detect(frame);
+                Barcode thisCode = barCodes.valueAt(0);
+                textView.setText(thisCode.rawValue);
+            }
+        });
+
     }
 }
